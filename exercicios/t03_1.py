2ccd2583718255da0ba14e5d6ff7da8ce34ee2d6
Valdir Jr <vsj@ic.ufal.br>
2019-10-25 10:54:55 -0300

    tarefa sendo feita

--- exercicios/t03_1.py
+++ exercicios/t03_1.py
@@ -3,18 +3,49 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def cropimread(img, xcrop, ycrop):
-    "Function to crop center of an image file"
-    ysize, xsize, chan = img.shape
-    xoff = (xsize - xcrop) // 2
-    yoff = (ysize - ycrop) // 2
+def cropCenter(img, x_center, y_center):
+    y, x, c = img.shape
+    xoff = (x - x_center) // 2
+    yoff = (y - y_center) // 2
     img2= img[yoff:-yoff,xoff:-xoff]
     return img2
 
+def swap(a,b):
+    return b,a
+
+def flipImg(img):
+    height = width = img.shape[0]
+    blank_image = np.zeros((height,width,3), np.uint8)
+    for i in range(img.shape[0]):  #x
+        for j in range(img.shape[1]): #y
+             blank_image[i][j] = img[i][width-1-j]
+
+    return blank_image
+        
+
 imfile = '../db/lena.png'
 img = cv2.imread(imfile)
-im_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img2= cropimread(im_rgb,200,200)
-plt.imshow(img2)
-#Crop and Flip
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)       
+#img2= cropimread(im_rgb,200,200)
+img_flipped = flipImg(img)
+img_cropped = cropCenter(img, 200, 200)
+
+plt.figure(figsize=(8,8))
+plt.subplot(221)
+plt.title("Original")
+plt.imshow(img)
+
+plt.subplot(222)
+plt.title("Flipped")
+plt.imshow(img_flipped)
+
+plt.subplot(223)
+plt.title("Original")
+plt.imshow(img)
+
+plt.subplot(224)
+plt.title("Cropped")
+plt.imshow(img_cropped)
+#plt.imshow(img2)
+#Crop fli
 
---
+++ exercicios/t03_2.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+#Image translation using numpy and openCv
+https://circuitdigest.com/tutorial/image-manipulation-in-python-opencv-part1
